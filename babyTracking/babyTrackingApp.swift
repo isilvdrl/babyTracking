583e195a8a04ea851f7d104f2eccfa090c26aa13
ışıl vardarlı <isil.vardarli@ogr.iuc.edu.tr>
2024-10-24 14:40:17 +0300

    Initial Commit

---
+++ babyTracking/babyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  babyTrackingApp.swift
+//  babyTracking
+//
+//  Created by IŞIL VARDARLI on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct babyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
